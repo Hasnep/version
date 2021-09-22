61b871acc4cfec5305353dbc5374cc11532daaca
Hannes <h@nnes.dev>
2021-09-22 23:33:18 +0100

    Add unwrap to build to silence warning

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ use std::collections::HashMap;
 
 fn main() {
     let tools: HashMap<&str, &str> = serde_json::from_str(include_str!("data/tools.json")).unwrap();
-    uneval::to_out_dir(tools, "tools.rs");
+    uneval::to_out_dir(tools, "tools.rs").unwrap();
 }
